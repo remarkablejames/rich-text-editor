6b0120ea24f4f48dee2061cf6d344d468c757cc0
James Niyongira <lascasasjames85@gmail.com>
2025-08-25 19:03:47 -0400

    feat: integrate RichTextRenderEngine and update styling for RichText components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { useEditorState } from '@/hooks/useEditorState';
+import { useEditorState } from "@/hooks/useEditorState";
 
-export { default } from '@/components/RichTextEditor';
+export { default } from "@/components/RichTextEditor";
+export { default as RichTextRenderEngine } from "@/components/RichTextRenderEngine";
 
-export type { UseEditorStateReturn } from '@/hooks/useEditorState';
+export type { RichTextEditorProps } from "@/components/RichTextEditor";
+export type { RichTextRenderEngineProps } from "@/components/RichTextRenderEngine";
+export type { UseEditorStateReturn } from "@/hooks/useEditorState";
 export { useEditorState };
-export { BubbleMenu } from '@tiptap/react';
-export type { Editor, UseEditorOptions } from '@tiptap/react';
-export * from './extensions/BaseKit';
+export { BubbleMenu } from "@tiptap/react";
+export type { Editor, UseEditorOptions } from "@tiptap/react";
+export * from "./extensions/BaseKit";
